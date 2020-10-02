9cfae43abb942583e7346d26ba127a7fb994739b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-02 18:35:03 +0200

    feat: iterate through death causes and compare them to death message

--- src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/listeners/DeathListener.kt
+++ src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/listeners/DeathListener.kt
@@ -1,6 +1,7 @@
 package com.github.DarkVanityOfLight.NiceDeaths.listeners
 
 import com.github.DarkVanityOfLight.NiceDeaths.Main
+import com.github.DarkVanityOfLight.NiceDeaths.enums.DeathCauses
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -9,6 +10,14 @@ class DeathListener(val main : Main) : Listener {
 
     @EventHandler
     fun onDeath(event: PlayerDeathEvent){
+
+        DeathCauses.values().forEach { DeathCause ->
+            event.deathMessage?.let { deathMessage ->
+                if (DeathCause.matches(deathMessage)){
+
+                }
+            }
+        }
 
     }
 }
