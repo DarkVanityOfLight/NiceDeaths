e0160d5fb1f546d012e2a85a6799f0b6cf6a338f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-04 20:01:05 +0200

    feat: parse death messages per faction

--- src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/parser/ConfigParser.kt
+++ src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/parser/ConfigParser.kt
@@ -5,7 +5,7 @@ import com.github.DarkVanityOfLight.NiceDeaths.enums.DeathCauses
 import com.github.DarkVanityOfLight.NiceDeaths.enums.ICauseDeath
 
 class ConfigParser(private val main: Main) {
-    var deathMessages : MutableMap<ICauseDeath, Array<String>> = emptyMap<ICauseDeath, Array<String>>().toMutableMap()
+    var deathMessages : MutableMap<String, MutableMap<ICauseDeath, Array<String>>> = emptyMap<String, MutableMap<ICauseDeath, Array<String>>>().toMutableMap()
 
     fun read(){
         main.saveDefaultConfig()
@@ -13,12 +13,15 @@ class ConfigParser(private val main: Main) {
 
         val config = main.config
 
-        deathMessages = emptyMap<ICauseDeath, Array<String>>().toMutableMap()
-        DeathCauses.values().forEach { deathCause ->
-            val messages: Array<String> = config.getStringList(deathCause.name).toTypedArray<String>()
-            if (messages.isNotEmpty()){
-                deathMessages[deathCause] = messages
+
+        deathMessages = emptyMap<String, MutableMap<ICauseDeath, Array<String>>>().toMutableMap()
+        for(faction : String in config.getConfigurationSection("messageMapping")?.getKeys(false)!!){
+            val factionDeathMessage : MutableMap<ICauseDeath, Array<String>> = emptyMap<ICauseDeath, Array<String>>().toMutableMap()
+            DeathCauses.values().forEach { deathCause ->
+                val messages : Array<String> = config.getStringList("messageMapping$faction.${deathCause.name}").toTypedArray<String>()
+                factionDeathMessage[deathCause] = messages
             }
+            deathMessages[faction] = factionDeathMessage
         }
     }
 }
