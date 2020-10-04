23734a48504e438f593dda76032ecd1f6d476f4c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-04 16:33:54 +0200

    feat: Parse death messages from config

--- src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/parser/ConfigParser.kt
+++ src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/parser/ConfigParser.kt
@@ -1,13 +1,24 @@
 package com.github.DarkVanityOfLight.NiceDeaths.parser
 
 import com.github.DarkVanityOfLight.NiceDeaths.Main
+import com.github.DarkVanityOfLight.NiceDeaths.enums.DeathCauses
+import com.github.DarkVanityOfLight.NiceDeaths.enums.ICauseDeath
 
 class ConfigParser(private val main: Main) {
+    var deathMessages : MutableMap<ICauseDeath, Array<String>> = emptyMap<ICauseDeath, Array<String>>().toMutableMap()
 
     fun read(){
         main.saveDefaultConfig()
         main.reloadConfig()
 
         val config = main.config
+
+        deathMessages = emptyMap<ICauseDeath, Array<String>>().toMutableMap()
+        DeathCauses.values().forEach { deathCause ->
+            val messages: Array<String> = config.getStringList(deathCause.name).toTypedArray<String>()
+            if (messages.isNotEmpty()){
+                deathMessages[deathCause] = messages
+            }
+        }
     }
 }
