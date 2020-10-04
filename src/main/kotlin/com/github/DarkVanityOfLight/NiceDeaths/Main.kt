de271c763db509af01e5f5c7850b2e04b49d4e39
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-04 16:35:27 +0200

    feat: read the config

--- src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/Main.kt
+++ src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/Main.kt
@@ -1,12 +1,15 @@
 package com.github.DarkVanityOfLight.NiceDeaths
 
 import com.github.DarkVanityOfLight.NiceDeaths.listeners.DeathListener
+import com.github.DarkVanityOfLight.NiceDeaths.parser.ConfigParser
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
+    val configParser: ConfigParser = ConfigParser(this)
 
     override fun onEnable() {
+        configParser.read()
         Bukkit.getPluginManager().registerEvents(DeathListener(this), this)
     }
 }
