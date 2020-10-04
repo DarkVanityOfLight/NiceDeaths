cef10b09d5886e7000da30f2b66c4348abb95f84
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-10-04 19:09:23 +0200

    feat: replace the formating values with the values from death message

--- src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/listeners/DeathListener.kt
+++ src/main/kotlin/com/github/DarkVanityOfLight/NiceDeaths/listeners/DeathListener.kt
@@ -16,7 +16,22 @@ class DeathListener(val configParser: ConfigParser) : Listener {
         DeathCauses.values().forEach { deathCause ->
             event.deathMessage?.let { deathMessage ->
                 if (deathCause.matches(deathMessage)){
-                    event.deathMessage = configParser.deathMessages[deathCause]?.get(0)
+                    var deathMessage = configParser.deathMessages[deathCause]?.get(0)
+
+                    // Check if deadPlayer is null and if yes just replace with ""
+                    val deadPlayer : String = if (deathCause.findDeadPlayer(event.deathMessage) == null) ""
+                    else deathCause.findDeadPlayer(event.deathMessage)!!
+                    deathMessage = deathMessage?.replace("{player}", deadPlayer)
+
+                    val killer : String = if (deathCause.findKiller(event.deathMessage) == null) ""
+                    else deathCause.findKiller(event.deathMessage)!!
+                    deathMessage?.replace("{killer}", killer)
+
+                    val weapon : String = if (deathCause.findWeapon(event.deathMessage) == null) ""
+                    else deathCause.findWeapon(event.deathMessage)!!
+                    deathMessage?.replace("{weapon}", weapon)
+
+                    event.deathMessage = deathMessage
                     return
                 }
             }
